a4ca63b997af09ae98d8f277742e31fdd4c6a8aa
SynScoots <admin@winterwind.co.uk>
2025-05-21 15:31:47 +0100

    Remove debug

--- src/ScootsCraft/ScootsCraft.lua
+++ src/ScootsCraft/ScootsCraft.lua
@@ -2004,38 +2004,3 @@ ScootsCraft.frames.events:RegisterEvent('TRADE_SKILL_SHOW')
 ScootsCraft.frames.events:RegisterEvent('TRADE_SKILL_UPDATE')
 ScootsCraft.frames.events:RegisterEvent('TRADE_SKILL_CLOSE')
 ScootsCraft.frames.events:RegisterEvent('PLAYER_LEAVING_WORLD')
-
-
-
-function dumpvar(data)
-    -- cache of tables already printed, to avoid infinite recursive loops
-    local tablecache = {}
-    local buffer = ""
-    local padder = "    "
- 
-    local function _dumpvar(d, depth)
-        local t = type(d)
-        local str = tostring(d)
-        if (t == "table") then
-            if (tablecache[str]) then
-                -- table already dumped before, so we dont
-                -- dump it again, just mention it
-                buffer = buffer.."<"..str..">\n"
-            else
-                tablecache[str] = (tablecache[str] or 0) + 1
-                buffer = buffer.."("..str..") {\n"
-                for k, v in pairs(d) do
-                    buffer = buffer..string.rep(padder, depth+1).."["..k.."] => "
-                    _dumpvar(v, depth+1)
-                end
-                buffer = buffer..string.rep(padder, depth).."}\n"
-            end
-        elseif (t == "number") then
-            buffer = buffer.."("..t..") "..str.."\n"
-        else
-            buffer = buffer.."("..t..") \""..str.."\"\n"
-        end
-    end
-    _dumpvar(data, 0)
-    return buffer
-end
